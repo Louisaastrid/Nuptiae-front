6766033f2d67a5dfe07f841712e112151b109b30
Louisa <rangbaastrid@gmail.com>
2021-02-21 18:03:02 +0100

    finished navbar nuptiae add view page

--- src/App.js
+++ src/App.js
@@ -4,18 +4,29 @@ import Header from './components/header/header'
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Footer from './components/footer/footer'
 import Navigation from './components/Navigation/navigation'
-import  SearchInpput from "./components/SearchBarre/search";
 import Slider from "./components/Slider/slider";
+
+import  SearchInpput from "./components/SearchBarre/search";
+import accueil from "./views/accueil";
+import listeDeNoce from "./views/listeDeNoce";
+import blog from "./views/blog";
+import activites from "./views/activites";
 function App() {
   return (
     <Router> 
     <div className="App">
       <Header/>
       <Navigation/>
-      <Slider/>
+            <Slider/>
+
+
        {/* <Destinations/> */}
+        <Route path="/" component={accueil} />
         <Route path="/destinations" component={Destinations} />
-         <Route path="/search" component={SearchInpput} />
+        <Route path="/liste" component={listeDeNoce}  />
+        <Route path="/activites" component={activites}  />
+        <Route path="/bloc" component={blog}  />
+        <Route path="/search" />
 
       <Footer/>
     </div>
